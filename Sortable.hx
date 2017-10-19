34e96e3828b54cec1b781b6e25fa69de19b50a04
sam <sam@silver42.karoo.co.uk>
2017-10-19 02:05:02 +0100

    Minor doc/wording tweaks

--- Sortable.hx
+++ Sortable.hx
@@ -1,8 +1,8 @@
 package;
 
+// Externs for sortable.js, a drop-in script to make tables sortable (https://github.com/HubSpot/sortable)
 @:native("Sortable")
-extern class Sortable
-{
+extern class Sortable {
 	public static function init():Void;
 	public static function initTable(id:String):Void;
 }
